46c89be275aafbfe2139861b3cfed849f0b350fe
tornapi <admin@torncity.pro>
2022-02-19 20:31:47 -0800

    Fix bank rates

--- TornJson/TornData/BankRates.cs
+++ TornJson/TornData/BankRates.cs
@@ -22,14 +22,14 @@ namespace TornJson.TornData
 {
     public class BankRates
     {
-        [JsonProperty("1w")] public string OneWeek { get; set; }
+        [JsonProperty("1w")] public double OneWeek { get; set; }
 
-        [JsonProperty("2w")] public string TwoWeeks { get; set; }
+        [JsonProperty("2w")] public double TwoWeeks { get; set; }
 
-        [JsonProperty("1m")] public string OneMonth { get; set; }
+        [JsonProperty("1m")] public double OneMonth { get; set; }
 
-        [JsonProperty("2m")] public string TwoMonths { get; set; }
+        [JsonProperty("2m")] public double TwoMonths { get; set; }
 
-        [JsonProperty("3m")] public string ThreeMonths { get; set; }
+        [JsonProperty("3m")] public double ThreeMonths { get; set; }
     }
 }
